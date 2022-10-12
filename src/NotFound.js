4988dee16fa781a307236fa749bcffc03afdd24e
Saku Suorajärvi <saku.suorajarvi@hotmail.fi>
2022-10-12 21:59:54 +0300

    joo

---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+    return(
+
+        <div>
+            This is NotFound page
+        </div>
+    )
+}
---
+++ src/components/ContactUs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+    return(
+
+        <div>
+            This is Contact Us page
+        </div>
+    )
+}
